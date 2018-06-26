7d6d89eadeb368c48110667d0dec0e32df99ee92
otacky_taka <fukuda@waremo.com>
2018-06-26 11:47:06 +0900

    Initial commit.  Section 2-3 of Syoda/18

---
+++ hello/index.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import HelloForm
+
+class HelloView(TemplateView):
+    
+    def __init__(self):
+        self.params = {
+                'title': 'Hello',
+                'message': 'your data:',
+                'form': HelloForm()
+            }
+    
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+
+    def post(self, request):
+        msg = 'あなたは、<b>' + request.POST['name'] + \
+            '（' + request.POST['age'] + \
+            '）</b>さんです。<br>メールアドレスは <b>' + request.POST['mail'] + \
+            '</b> ですね。'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
---
+++ hello/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from .views import HelloView
+
+urlpatterns = [
+    url(r'', HelloView.as_view(), name='index'),
+]
