e18f95e390f8c356d70bde14204c0fc5ff932369
otacky_taka <fukuda@waremo.com>
2018-06-27 19:39:17 +0900

    Section 3-4

--- hello/forms.py
+++ hello/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from .models import Friend
+
 
 class HelloForm(forms.Form):
     name = forms.CharField(label='name')
     mail = forms.CharField(label='mail')
     age = forms.IntegerField(label='age')
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Friend
-from .forms import HelloForm
+from .forms import FriendForm, HelloForm #この文を新たに追記
+
+def create(request):
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
+    return render(request, 'hello/create.html', params)
 
 def index(request):
     params = {
@@ -23,3 +30,28 @@ def index(request):
     else:
         params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id':num,
+        'form': FriendForm(instance=obj),
+    }
+    return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id':num,
+        'obj': friend,
+    }
+    return render(request, 'hello/delete.html', params)
