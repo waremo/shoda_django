368e0901aad08ca5f0c7bb3add7353a92e16fca0
otacky_taka <fukuda@waremo.com>
2018-06-27 15:57:47 +0900

    chap3. to fix /static/

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
-from .views import HelloView
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    url(r'', HelloView.as_view(), name='index'),
+    path('', views.index, name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,23 +3,23 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .forms import HelloForm
 
-class HelloView(TemplateView):
-    
-    def __init__(self):
-        self.params = {
-                'title': 'Hello',
-                'message': 'your data:',
-                'form': HelloForm()
-            }
-    
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Friend
+from .forms import HelloForm
 
-    def post(self, request):
-        msg = 'あなたは、<b>' + request.POST['name'] + \
-            '（' + request.POST['age'] + \
-            '）</b>さんです。<br>メールアドレスは <b>' + request.POST['mail'] + \
-            '</b> ですね。'
-        self.params['message'] = msg
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
+def index(request):
+    params = {
+            'title': 'Hello',
+            'message': 'all friends.',
+            'form':HelloForm(),
+            'data': [],
+        }
+    if (request.method == 'POST'):
+        num=request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
+    return render(request, 'hello/index.html', params)
